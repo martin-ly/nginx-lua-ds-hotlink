8fcbd4824e77c06c3c825f0a1b78be855a30ec8d
Hevienz <hevienz@gmail.com>
2014-06-03 09:36:22 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,9 +2,11 @@ module("config")
 
 logs_pwd="/var/log/hotlink/"
 
+----refer module----
 white_domains={
     "geekhub\\.cc",
 }
 
+----access module----
 secret_key="naudw72h2iu34298dnawi81"
 expiration_time=3600
